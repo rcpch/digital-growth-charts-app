57c0267fe64537acd745c7ef6317c1ab8b7e6dbf
Michael Barton <michael.barton@rcpch.ac.uk>
2025-06-25 15:31:41 +0100

    Add dummy test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,19 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:digital_growth_charts_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Dummy test app mounts', (WidgetTester tester) async {
+    await dotenv.load();
+
     // Build our app and trigger a frame.
     await tester.pumpWidget(const DGCApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Dummy test to check everything is mounted
+    expect(find.text('RCPCH Digital Growth Charts'), findsOneWidget);
   });
 }
