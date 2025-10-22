abbc1907d2284acbadb82169f3b790e485b573da
Michael Barton <michael.barton@rcpch.ac.uk>
2025-10-22 14:01:56 +0100

    Switch to named tabs

--- lib/widgets/results.dart
+++ lib/widgets/results.dart
@@ -2,9 +2,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-// Third party imports
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-
 // RCPCH Imports
 import '../definitions/enums.dart';
 import './centile_chart.dart';
@@ -20,90 +17,56 @@ class ResultsPage extends StatefulWidget {
   State<ResultsPage> createState() => _ResultsPageState();
 }
 
-class _ResultsPageState extends State<ResultsPage> {
-  late PageController _pageController;
-  late int _currentPage;
+enum ResultsTab {
+  data,
+  height,
+  weight,
+  ofc
+}
 
-  @override
-  void initState() {
-    super.initState();
-
-    var appState = context.read<AppState>();
-
-    if (widget.initialMeasurementMethod != null &&
-        appState.organizedGrowthData.containsKey(
-          widget.initialMeasurementMethod,
-        )) {
-      // Start on the page for the specified measurement method
-      var methodIndex = appState.organizedGrowthData.keys.toList().indexOf(
-        widget.initialMeasurementMethod!,
-      );
-      _currentPage = methodIndex;
-      _pageController = PageController(initialPage: methodIndex);
-    } else {
-      // Default to the first chart page
-      _currentPage = 0;
+class _ResultsPageState extends State<ResultsPage> with SingleTickerProviderStateMixin {
+  Widget buildChart(MeasurementMethod method, AppState appState) {
+    if(!appState.organizedGrowthData.containsKey(method)) {
+      return Center(child: Text("No growth data available"));
     }
 
-    _pageController = PageController(initialPage: _currentPage);
-  }
-
-  // Function to navigate to a specific page
-  void _goToPage(int page) {
-    _pageController.animateToPage(
-      page,
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeInOut,
+    return CentileChart(
+      organizedCentileLines: appState.organizedCentileLines,
+      measurementMethod: method,
+      sex: appState.sex!,
+      growthDataForMethod: appState.organizedGrowthData[method]!,
+      dob: appState.dob!,
+      gestationWeeks: appState.gestationWeeks,
+      gestationDays: appState.gestationDays,
     );
   }
 
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
-
-  Widget _buildPageView(AppState appState) {
-    var numPages =
-        appState.organizedGrowthData.length +
-        (appState.organizedGrowthData.isNotEmpty ? 1 : 0);
-
-    return PageView.builder(
-      key: const ValueKey('pageView'),
-      controller: _pageController,
-      itemCount: numPages,
-      itemBuilder: (context, index) {
-        if (index == numPages - 1 && numPages > 1) {
-          // Last page is the data table
-          return ResultsDataTable(
-            key: const PageStorageKey('data_table'),
-            organizedGrowthData: appState.organizedGrowthData,
-          );
-        } else if (appState.dob != null && appState.sex != null) {
-          var method = appState.organizedGrowthData.keys.elementAt(index);
-
-          return CentileChart(
-            key: PageStorageKey('chart_$method'),
-            organizedCentileLines: appState.organizedCentileLines,
-            measurementMethod: method,
-            sex: appState.sex!,
-            growthDataForMethod: appState.organizedGrowthData[method]!,
-            dob: appState.dob!,
-            gestationWeeks: appState.gestationWeeks,
-            gestationDays: appState.gestationDays,
-          );
-        }
-
-        return const Center(
-          child: Text('Cannot display chart. Missing date of birth or sex.'),
+  Widget buildTab(ResultsTab tab, AppState appState) {
+    switch (tab) {
+      case ResultsTab.data:
+        return ResultsDataTable(
+          organizedGrowthData: appState.organizedGrowthData,
         );
-      },
-      onPageChanged: (index) {
-        setState(() {
-          _currentPage = index;
-        });
-      },
-    );
+      case ResultsTab.height:
+        return buildChart(MeasurementMethod.height, appState);
+      case ResultsTab.weight:
+        return buildChart(MeasurementMethod.weight, appState);
+      case ResultsTab.ofc:
+        return buildChart(MeasurementMethod.ofc, appState);
+    }
+  }
+
+  Tab buildTabLink(ResultsTab tab) {
+    switch (tab) {
+      case ResultsTab.data:
+        return const Tab(icon: Icon(Icons.child_care));
+      case ResultsTab.height:
+        return const Tab(text: 'Height');
+      case ResultsTab.weight:
+        return const Tab(text: 'Weight');
+      case ResultsTab.ofc:
+        return const Tab(text: 'Head Cm.');
+    }
   }
 
   @override
@@ -121,57 +84,37 @@ class _ResultsPageState extends State<ResultsPage> {
       );
     }
 
-    // Calculate the total number of pages (charts + data table)
-    int numPages =
-        appState.organizedGrowthData.length +
-        (appState.organizedGrowthData.isNotEmpty ? 1 : 0);
-    // Only add the data table page if there is at least one chart
+    // Default to the first tab with charts
+    var currentTab = 1;
+
+    if (widget.initialMeasurementMethod != null &&
+        appState.organizedGrowthData.containsKey(
+          widget.initialMeasurementMethod,
+        )) {
+      // Start on the tab for the specified measurement method
+      final measurementMethodIndex = appState.organizedGrowthData.keys.toList().indexOf(
+        widget.initialMeasurementMethod!,
+      );
+      
+      currentTab = measurementMethodIndex + 1;
+    }
 
     return Scaffold(
       appBar: AppBar(title: const Text('Growth Chart Results')),
-      body: Column(
-        children: [
-          Expanded(child: _buildPageView(appState)),
-          if (appState.organizedGrowthData.isNotEmpty)
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Column(
-                children: [
-                  SmoothPageIndicator(
-                    controller: _pageController,
-                    count: numPages,
-                    effect: WormEffect(
-                      dotColor: Colors.grey,
-                      activeDotColor: Theme.of(context).primaryColor,
-                      dotHeight: 8.0,
-                      dotWidth: 8.0,
-                      spacing: 5.0,
-                    ),
-                    onDotClicked: (index) {
-                      _goToPage(index);
-                    },
-                  ),
-                  const SizedBox(height: 8.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      TextButton(
-                        onPressed: _currentPage > 0 ? () => _goToPage(0) : null,
-                        child: const Text('Chart'),
-                      ),
-                      TextButton(
-                        onPressed: _currentPage < numPages - 1
-                            ? () => _goToPage(numPages - 1)
-                            : null,
-                        child: const Text('Details'),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-        ],
-      ),
+      body: DefaultTabController(
+        length: ResultsTab.values.length,
+        initialIndex: currentTab,
+        child: Scaffold(
+          body: TabBarView(
+            children: [for (var tab in ResultsTab.values) buildTab(tab, appState)]
+          ),
+          bottomNavigationBar: TabBar(
+            isScrollable: true,
+            tabAlignment: TabAlignment.start,
+            tabs: [for (var tab in ResultsTab.values) buildTabLink(tab)]
+          ),
+        )
+      )
     );
   }
 }
