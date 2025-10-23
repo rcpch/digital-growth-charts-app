c1a0342a38a3b27b09a92a1fb919b20a1f366d61
Michael Barton <michael.barton@rcpch.ac.uk>
2025-10-23 11:45:48 +0100

    Settings page

---
+++ lib/widgets/settings.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('RCPCH Growth Charts')),
+      body: const Center(child: Text('Settings Page')),
+    );
+  }
+}
